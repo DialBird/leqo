1665738326411824de30a3dc975eea7bc910f622
Keisuke Taniguchi <rememberthatk@icloud.com>
2023-05-31 17:29:05 +0900

    Initial Commit

---
+++ leqo/leqoApp.swift
@@ -0,0 +1,17 @@
+//
+//  leqoApp.swift
+//  leqo
+//
+//  Created by 谷口敬亮 on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct leqoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
